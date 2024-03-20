ca98a61ebe078c1cab857c9fc7d610573dab6135
yorunoken <yorunoken@skiff.com>
2024-03-20 20:07:41 +0300

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { images: { domains: ["cdn.discordapp.com"] } };
+const nextConfig = { images: { remotePatterns: ["cdn.discordapp.com"] } };
 
 export default nextConfig;
