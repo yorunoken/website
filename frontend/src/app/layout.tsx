279528f2c7c66039173126218c0f185e60a90bef
Yoru <m.f.fetvaci@hotmail.com>
2024-10-05 00:46:59 +0300

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
 import { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
     title: "Yoru portfolio",
@@ -29,6 +30,11 @@ export default function RootLayout({
                     <main className="flex-grow mx-auto w-full">{children}</main>
                     <Footer />
                 </ThemeProvider>
+                <Script
+                    src="https://cloud.umami.is/script.js"
+                    data-website-id="76d0692e-1bef-4dcb-9b51-78f87d803f34"
+                    strategy="afterInteractive"
+                />
             </body>
         </html>
     );
